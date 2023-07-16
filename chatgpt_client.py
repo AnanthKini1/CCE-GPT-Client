fc311bcfbb85d2e9b2212abd00bd935c925d2b73
Ananth Kini <ananthkini1@gmail.com>
2023-07-16 16:39:49 -0700

    adding text to speech

--- chatgpt_client.py
+++ chatgpt_client.py
@@ -1,5 +1,6 @@
 import os
 import openai 
+from gtts import gTTS
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,18 +8,29 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAPI_KEY")
 
 question = input("What is your question for BridgeHealth? ")
+def ask_question():
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages = [
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": f"{question}"},
+        ]
+    )
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages = [
-        {"role": "system", "content": "You are a chatbot"},
-        {"role": "user", "content": f"{question}"},
-    ]
-)
+    result = ''
+    for choice in response.choices:
+        result += choice.message.content
 
-result = ''
-for choice in response.choices:
-    result += choice.message.content
+    print(f"We asked BridgeHealth {question} - here is their response")
+    print(result)
+    return (result)
 
-print(f"We asked BridgeHealth {question} - here is their response")
-print(result)
+def create_mp3(result):
+    language = 'en'
+    mp3 = gTTS(text = f"{result}", lang = language)
+    mp3.save(f'{ question }.mp3')
+
+answer = ask_question()
+create_mp3(answer)
+
+
---
+++ text_to_speech.py
@@ -0,0 +1,9 @@
+from gtts import gTTS
+transformToMP3 = input("What do you want transformed to an MP3? ")
+
+def create_mp3(result):
+    language = 'en'
+    mp3 = gTTS(text = f"{result}", lang = language)
+    mp3.save('Text_To_Speech.mp3')
+
+create_mp3(transformToMP3)
